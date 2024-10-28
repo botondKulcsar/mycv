21ebc962e4b200549ca927d34fdf891a1633233a
Botond KULCSAR <bkulcsar@gmx.com>
2024-10-28 07:46:22 +0200

    add ormconfig.ts file

---
+++ src/ormconfig.ts
@@ -0,0 +1,19 @@
+import { DataSourceOptions, DataSource } from 'typeorm';
+
+const dbConfig = {
+  type: 'sqlite',
+  database: process.env.NODE_ENV == 'development' ? 'db.sqlite' : 'test.sqlite',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: process.env.NODE_ENV == 'development' ? false : true,
+} as DataSourceOptions;
+
+export const AppDataSource = new DataSource(dbConfig);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initiliazed');
+    console.log(dbConfig);
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err.message);
+  });
--- test/setup.ts
+++ test/setup.ts
@@ -4,5 +4,7 @@ import { join } from 'path';
 global.beforeEach(async () => {
   try {
     await rm(join(__dirname, '..', 'test.sqlite'));
-  } catch (error) {}
+  } catch (error) {
+    console.error('error occured while deleting test.sqlite:', error.message);
+  }
 });
